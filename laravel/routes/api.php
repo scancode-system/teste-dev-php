2f69811c6fc6e89f63011bce99765a3c1bdc1d5d
Your Name <you@example.com>
2021-02-16 14:17:58 -0300

    criacao das rotas

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\CarroController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/carros', [CarroController::class, 'index']);
+Route::post('/carros', [CarroController::class, 'store']);
+Route::get('/carros/{id}', [CarroController::class, 'edit']);
+Route::put('/carros/{id}', [CarroController::class, 'update']);
+Route::delete('/carros/{id}', [CarroController::class, 'destroy']);
