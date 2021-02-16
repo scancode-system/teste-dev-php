bc77a4f809b7b0d3a1977d1b1a3982754b26063c
Your Name <you@example.com>
2021-02-16 13:58:59 -0300

    cricação do projeto laravel e migrations

---
+++ laravel/database/migrations/2021_02_16_165031_create_carros_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carros', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('marca_id');
+            $table->foreign('marca_id')->references('id')->on('marcas')->onUpdate('cascade')->onDelete('restrict');
+            $table->string('modelo');
+            $table->integer('ano');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carros');
+    }
+}
