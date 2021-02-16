a3d3b8ae4e67eaf083aeedf0d7bf349ddef82577
Your Name <you@example.com>
2021-02-16 14:32:57 -0300

    criação da API

--- laravel/app/Http/Controllers/Api/CarroController.php
+++ laravel/app/Http/Controllers/Api/CarroController.php
@@ -3,9 +3,35 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Carro;
 use Illuminate\Http\Request;
 
 class CarroController extends Controller
 {
-    //
+    
+    public function index(){
+        return Carro::all();
+    }
+
+    public function store(Request $request){
+        $carro = Carro::create($request->all());
+        return $carro;
+    }
+
+    public function edit(Request $request, $id){
+        return Carro::find($id);
+    }
+
+    public function update(Request $request, $id){
+        $carro = Carro::find($id);
+        $carro->update($request->all());
+        return $carro;
+    }
+
+    public function delete(Request $request, $id){
+        $carro = Carro::find($id);
+        $carro->delete();
+        return 'Carro deletado com sucesso';
+    }
+
 }
--- laravel/app/Models/Carro.php
+++ laravel/app/Models/Carro.php
@@ -2,12 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Marca;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Carro extends Model
 {
     use HasFactory;
 
     protected $fillable = ['marca_id', 'modelo', 'ano'];
+
+    public function marca()
+    {
+        return $this->belongsTo(Marca::class);
+    }
+
 }
